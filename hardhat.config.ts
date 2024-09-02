28de215a87e7554333977e9c13f511d89e1162ca
frankcrypto <frankcrypto@gmail.com>
2024-09-02 14:05:26 +0800

    restore format

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,88 +1,82 @@
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import { HardhatUserConfig } from "hardhat/config";
-import "hardhat-deploy";
-import "@nomiclabs/hardhat-etherscan";
+import '@nomiclabs/hardhat-waffle'
+import '@typechain/hardhat'
+import { HardhatUserConfig, task } from 'hardhat/config'
+import 'hardhat-deploy'
+import '@nomiclabs/hardhat-etherscan'
 
-import "solidity-coverage";
+import 'solidity-coverage'
 
-import * as fs from "fs";
+import * as fs from 'fs'
 require("dotenv").config();
+const SALT = '0x90d8084deab30c2a37c45e8d47f49f2f7965183cb6990a98943ef94940681de3'
+process.env.SALT = process.env.SALT ?? SALT
 const qngRpc = process.env.QNG_RPC ?? "http://localhost:8545";
-const mnemonicFileName =
-  process.env.MNEMONIC_FILE ??
-  `${process.env.HOME}/.secret/testnet-mnemonic.txt`;
-let mnemonic = "test ".repeat(11) + "junk";
-if (fs.existsSync(mnemonicFileName)) {
-  mnemonic = fs.readFileSync(mnemonicFileName, "ascii");
-}
+task('deploy', 'Deploy contracts')
+  .addFlag('simpleAccountFactory', 'deploy sample factory (by default, enabled only on localhost)')
 
-function getNetwork1(url: string): {
-  url: string;
-  accounts: { mnemonic: string };
-} {
+const mnemonicFileName = process.env.MNEMONIC_FILE!
+let mnemonic = 'test '.repeat(11) + 'junk'
+if (fs.existsSync(mnemonicFileName)) { mnemonic = fs.readFileSync(mnemonicFileName, 'ascii') }
+
+function getNetwork1 (url: string): { url: string, accounts: { mnemonic: string } } {
   return {
     url,
-    accounts: { mnemonic },
-  };
+    accounts: { mnemonic }
+  }
 }
 
-function getNetwork(name: string): {
-  url: string;
-  accounts: { mnemonic: string };
-} {
-  return getNetwork1(`https://${name}.infura.io/v3/${process.env.INFURA_ID}`);
+function getNetwork (name: string): { url: string, accounts: { mnemonic: string } } {
+  return getNetwork1(`https://${name}.infura.io/v3/${process.env.INFURA_ID}`)
   // return getNetwork1(`wss://${name}.infura.io/ws/v3/${process.env.INFURA_ID}`)
 }
 
 const optimizedComilerSettings = {
-  version: "0.8.17",
+  version: '0.8.23',
   settings: {
     optimizer: { enabled: true, runs: 1000000 },
-    viaIR: true,
-  },
-};
+    viaIR: true
+  }
+}
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
   solidity: {
-    compilers: [
-      {
-        version: "0.8.15",
-        settings: {
-          optimizer: { enabled: true, runs: 1000000 },
-        },
-      },
-    ],
+    compilers: [{
+      version: '0.8.23',
+      settings: {
+        optimizer: { enabled: true, runs: 1000000 }
+      }
+    }],
     overrides: {
-      "contracts/core/EntryPoint.sol": optimizedComilerSettings,
-      "contracts/samples/SimpleAccount.sol": optimizedComilerSettings,
-    },
+      'contracts/core/EntryPoint.sol': optimizedComilerSettings,
+      'contracts/samples/SimpleAccount.sol': optimizedComilerSettings
+    }
   },
   networks: {
-    dev: { url: "http://localhost:8545" },
+    dev: { url: 'http://localhost:8545' },
     // github action starts localgeth service, for gas calculations
-    localgeth: { url: "http://localgeth:8545" },
-    goerli: getNetwork("goerli"),
-    sepolia: getNetwork("sepolia"),
-    proxy: getNetwork1("http://localhost:8545"),
-    qng: getNetwork1(qngRpc),
+    localgeth: { url: 'http://localgeth:8545' },
+    goerli: getNetwork('goerli'),
+    sepolia: getNetwork('sepolia'),
+    proxy: getNetwork1('http://localhost:8545'),
+    qng: getNetwork1(qngRpc)
   },
   mocha: {
-    timeout: 10000,
+    timeout: 10000
   },
-
+  // @ts-ignore
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
-};
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
+
+}
 
 // coverage chokes on the "compilers" settings
 if (process.env.COVERAGE != null) {
   // @ts-ignore
-  config.solidity = config.solidity.compilers[0];
+  config.solidity = config.solidity.compilers[0]
 }
 
-export default config;
+export default config
