fffe9a4b227854629bab98e028ebf437958bd8d1
frankcrypto <frankcrypto@gmail.com>
2024-09-02 13:21:15 +0800

    restore format

--- deploy/2_deploy_SimpleAccountFactory.ts
+++ deploy/2_deploy_SimpleAccountFactory.ts
@@ -1,30 +1,29 @@
-import { HardhatRuntimeEnvironment } from "hardhat/types";
-import { DeployFunction } from "hardhat-deploy/types";
-import { ethers } from "hardhat";
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+import { ethers } from 'hardhat'
 
-const deploySimpleAccountFactory: DeployFunction = async function (
-  hre: HardhatRuntimeEnvironment
-) {
-  const provider = ethers.provider;
-  const from = await provider.getSigner().getAddress();
-  const network = await provider.getNetwork();
+const deploySimpleAccountFactory: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const provider = ethers.provider
+  const from = await provider.getSigner().getAddress()
+  const network = await provider.getNetwork()
   // only deploy on local test network.
   if (network.chainId !== 31337 && network.chainId !== 1337) {
     // can deploy qng testnet and privnet
     if (network.chainId != 8131 && network.chainId != 8133) {
-      return;
+	return
     }
   }
 
-  const entrypoint = await hre.deployments.get("EntryPoint");
-  const ret = await hre.deployments.deploy("SimpleAccountFactory", {
-    from,
-    args: [entrypoint.address],
-    gasLimit: 6e6,
-    log: true,
-    deterministicDeployment: true,
-  });
-  console.log("==SimpleAccountFactory addr=", ret.address);
-};
+  const entrypoint = await hre.deployments.get('EntryPoint')
+  const ret = await hre.deployments.deploy(
+    'SimpleAccountFactory', {
+      from,
+      args: [entrypoint.address],
+      gasLimit: 6e6,
+      log: true,
+      deterministicDeployment: true
+    })
+  console.log('==SimpleAccountFactory addr=', ret.address)
+}
 
-export default deploySimpleAccountFactory;
+export default deploySimpleAccountFactory
